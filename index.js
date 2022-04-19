c2f8869e66d632b7793f45bd7f776d23d499be5c
Decodedal <Palumbodallas@gmail.com>
2022-04-18 20:21:12 -0400

    got it ;)

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+
+let selectedColor = 'blue'
 const painting = document.querySelector('.painting')
 painting.addEventListener('click',function(e){
 
-e.target.style.backgroundColor = 'blue'
+e.target.style.backgroundColor = selectedColor
 }
-
-
 )
+painting.addEventListener('dblclick',function(m){
+m.target.style.backgroundColor = "white"
+}
+)
+
+
+let blue = document.querySelector("#blue")
+let red = document.querySelector("#red")
+let yellow = document.querySelector("#yellow")
+let green = document.querySelector("#green")
+
+blue.addEventListener('click',function(){
+selectedColor = "blue"}
+)
+red.addEventListener('click',function(){
+    selectedColor = "red"}
+    )
+    yellow.addEventListener('click',function(){
+        selectedColor = "yellow"}
+        )
+        green.addEventListener('click',function(){
+            selectedColor = "green"}
+            )
+
+
+
